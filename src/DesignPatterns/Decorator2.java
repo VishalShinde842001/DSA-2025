a8d1819cb29fe7d1a98aa8bb7c5d2c29530b1299
vishal shinde <vishalshinde0804@gmail.com>
2025-04-23 22:43:01 +0530

    Decorator Design Pattern code and something other 23-04-2025

--- src/DesignPatterns/Decorator2.java
+++ src/DesignPatterns/Decorator2.java
@@ -9,6 +9,11 @@ public class Decorator2 {
         Tea teaWithHalfLiter = new ExtraMilk(normal,500,40);
         System.out.println(teaWithHalfLiter.price());
         System.out.println(teaWithHalfLiter.description());
+
+        Tea teaWithExtraSugarAnMilk=new ExtraSugar(teaWithHalfLiter,500,.04);
+        System.out.println(teaWithExtraSugarAnMilk.price());
+        System.out.println(teaWithExtraSugarAnMilk.description());
+
     }
 }
 
@@ -76,5 +81,37 @@ class ExtraMilk extends TeaDecorator {
     @Override
     public String description() {
         return super.description()+" With Extra Milk";
+    }
+}
+
+// ---- Concrete Decorator: ExtraSugar ----
+ class ExtraSugar extends TeaDecorator {
+
+    private final int extraSugarQuantityInGrams;
+    private final double sugarPricePerGram;
+
+    /**
+     * @param tea                         the Tea to wrap
+     * @param extraSugarQuantityInGrams  amount of sugar in grams
+     * @param sugarPricePerGram          price for one gram of sugar
+     */
+    public ExtraSugar(Tea tea, int extraSugarQuantityInGrams, double sugarPricePerGram) {
+        super(tea);
+        this.extraSugarQuantityInGrams = extraSugarQuantityInGrams;
+        this.sugarPricePerGram         = sugarPricePerGram;
+    }
+
+    @Override
+    public double price() {
+        double basePrice = super.price();  // price of the wrapped Tea
+        double sugarCost = extraSugarQuantityInGrams * sugarPricePerGram;
+        return basePrice + sugarCost;
+    }
+
+    @Override
+    public String description() {
+        return super.description()
+                + " + Extra Sugar ("
+                + extraSugarQuantityInGrams + "g)";
     }
 }
