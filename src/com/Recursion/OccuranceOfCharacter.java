9f6e67f69ee254e57332ce7ecc89081bd4e1d4b4
vishal shinde <vishalshinde0804@gmail.com>
2025-05-08 21:48:01 +0530

    some recusion basic questions

---
+++ src/com/Recursion/OccuranceOfCharacter.java
@@ -0,0 +1,25 @@
+package com.Recursion;
+
+public class OccuranceOfCharacter {
+
+    public static void main(String[] args) {
+        System.out.println(occurance("occurance",'c'));
+    }
+
+    public static int occurance(String str, char target) {
+        return counter(str, 0, target);
+    }
+
+    public static int counter(String str, int count, char target) {
+        if (str.isEmpty()) {
+            return count;
+        }
+
+        char ch = str.charAt(0);
+        if (ch == target) {
+            return counter(str.substring(1), count + 1, target);
+        } else {
+            return counter(str.substring(1), count, target);
+        }
+    }
+}
